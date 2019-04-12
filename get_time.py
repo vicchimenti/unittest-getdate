16e9b4a208cc4324f500e2bfffc589b17bbc9abb
Victor Chimenti <chimenti@seattleu.edu>
2019-04-11 19:29:51 -0700

    initial

--- get_time.py
+++ get_time.py
@@ -1,7 +1,16 @@
 import sys
+import datetime
 
 
-def GetDate():
+def GetDate(date_in):
+    try:
+        datetime.datetime.strptime(date_in, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError('Invalid Date Format, enter format YYYYY-MM-DD')
     return True
 
-print(str(GetDate()))
+
+date_input = input("Enter any date: ")
+date_today = datetime.datetime.now()
+print(str(date_today))
+print(str(GetDate(date_input)))
