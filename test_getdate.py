f6ea60779c095da3361e037acd6b1461f72f33c8
Victor Chimenti <chimenti@seattleu.edu>
2019-04-16 19:10:19 -0700

    trying unittest again

--- test_getdate.py
+++ test_getdate.py
@@ -1,24 +1,30 @@
 from unittest import TestCase
+from get_time import getdate
 
 
 class TestGetdate(TestCase):
     def test_getdate(self):
-        self.assertTrue(2019-1-1)
-        self.assertFalse(2019-12-0)
-        self.assertFalse(2019-0-12)
-        self.assertTrue(2019-12-12)
-        self.assertFalse(2019-13-12)
-        self.assertTrue(2019-12-13)
-        self.assertFalse(2019-27-12)
-        self.assertTrue(2019-12-27)
-        self.assertFalse(2019-28-10)
-        self.assertTrue(2019-10-28)
-        self.assertFalse(2019-29-10)
-        self.assertTrue(2019-10-29)
-        self.assertFalse(2019-30-10)
-        self.assertTrue(2019-10-30)
-        self.assertFalse(2019-31-10)
-        self.assertTrue(2019-10-31)
-        self.assertFalse(2019-100-10)
-        self.assertFalse(2019-10-100)
-        self.fail(self)
+        self.assertTrue(getdate(2019-1-1))
+        self.assertFalse(getdate(2019-12-0))
+        self.assertFalse(getdate(2019-0-12))
+        # self.assertTrue(2019-12-12)
+        # self.assertFalse(2019-13-12)
+        # self.assertTrue(2019-12-13)
+        # self.assertFalse(2019-27-12)
+        # self.assertTrue(2019-12-27)
+        # self.assertFalse(2019-28-10)
+        # self.assertTrue(2019-10-28)
+        # self.assertFalse(2019-29-10)
+        # self.assertTrue(2019-10-29)
+        # self.assertFalse(2019-30-10)
+        # self.assertTrue(2019-10-30)
+        # self.assertFalse(2019-31-10)
+        # self.assertTrue(2019-10-31)
+        # self.assertFalse(2019-100-10)
+        # self.assertFalse(2019-10-100)
+        # self.fail(self)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
