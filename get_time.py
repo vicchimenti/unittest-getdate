2669460bdc48eb02b20097282195e283f847087f
Vic Chimenti <chimenti@seattleu.edu>
2019-04-20 18:33:00 -0700

    refactoring to place testSuite in a dicitonary and then iterate tests in a loop to save space. Getting TypeError - need to debug

--- get_time.py
+++ get_time.py
@@ -1,6 +1,4 @@
 #   Victor Chimenti
-#   CPSC 5210 Testing and Debugging
-#   19SQ Seattle University
 #   get_time.py
 #   A program to unit test a valid date format
 
@@ -12,12 +10,14 @@ def getdate(date_in):
     try:
         datetime.datetime.strptime(date_in, '%Y-%m-%d')
     except ValueError:
-        # print('Invalid Date Format, enter format YYYYY-MM-DD')
+        # print('Invalid Date Format, enter format YYYYY-MM-DD')  // user input commented out
         return False
     return True
 
 
 date_today = datetime.datetime.now()
 print(str(date_today))
+
+# // user input commented out
 # date_input = input("Enter any month and day in the format YYYY-MM-DD to verify if it is a correct date format: ")
 # print(str(getdate(date_input)) + '\n')
